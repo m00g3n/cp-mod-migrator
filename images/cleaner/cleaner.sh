14125f3c5fab9b42de4efa0fcf9b00082a7ce10f
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-01-24 15:58:12 +0100

    cleanup script development

--- images/cleaner/cleaner.sh
+++ images/cleaner/cleaner.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
 echo "Running Connectivity Proxy Cleanup script"
-kubectl get pods -n kyma-system
 
-kubectl delete statefulset connectivity-proxy -n kyma-system
+kubectl delete statefulset -n kyma-system connectivity-proxy
+kubectl delete deployment -n kyma-system connectivity-proxy-restart-watcher
+kubectl delete deployment -n kyma-system connectivity-proxy-sm-operator
+
+kubectl delete service -n kyma-system connectivity-proxy
+kubectl delete service -n kyma-system connectivity-proxy-smv
+kubectl delete service -n kyma-system connectivity-proxy-tunnel
+kubectl delete service -n kyma-system connectivity-proxy-tunnel-0
+kubectl delete service -n kyma-system connectivity-proxy-tunnel-healthcheck
+
+kubectl delete serviceaccount -n kyma-system connectivity-proxy-restart-watcher
+kubectl delete clusterrole -n kyma-system connectivity-proxy-restart-watcher
+kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-restart-watcher
+
+kubectl delete serviceaccount -n kyma-system connectivity-proxy-sm-operator
+kubectl delete clusterrole -n kyma-system connectivity-proxy-service-mappings
+kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-service-mappings
+
+kubectl delete configmap -n kyma-system connectivity-proxy
+kubectl delete configmap -n kyma-system connectivity-proxy-info
+
 
 exit 0
