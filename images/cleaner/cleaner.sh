50d0baf57ac8043b6909c0e9709547f6100625cd
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-01-25 12:58:59 +0100

    annotate existing Connectivity Proxy Service Mappings

--- images/cleaner/cleaner.sh
+++ images/cleaner/cleaner.sh
@@ -19,9 +19,36 @@ kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-restart-watc
 kubectl delete serviceaccount -n kyma-system connectivity-proxy-sm-operator
 kubectl delete clusterrole -n kyma-system connectivity-proxy-service-mappings
 kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-service-mappings
+kubectl delete mutatingwebhookconfiguration -n kyma-system connectivity-proxy-mutating-webhook-configuration
 
 kubectl delete configmap -n kyma-system connectivity-proxy
 kubectl delete configmap -n kyma-system connectivity-proxy-info
+
+# Delete all istio resources created by connectivity-proxy
+
+kubectl delete envoyfilter -n istio-system connectivity-proxy-custom-protocol
+kubectl delete certificate -n istio-system cc-certs
+kubectl delete gateway -n kyma-system kyma-gateway-cc
+kubectl delete virtualservice -n kyma-system cc-proxy
+kubectl delete virtualservice -n kyma-system cc-proxy-healthcheck
+kubectl delete destinationrule -n kyma-system connectivity-proxy-tunnel-0
+kubectl delete destinationrule -n kyma-system connectivity-proxy
+kubectl delete peerauthentication -n enable-permissive-mode-for-cp
+
+# Annotate all existing Connectivity Proxy Service Mappings
+
+mappings=$(kubectl get servicemappings.connectivityproxy.sap.com -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+
+for mapping in mappings; do
+
+  echo "Applying annotations to service mapping $mapping"
+
+  kubectl annotate servicemappings.connectivityproxy.sap.com "$mapping" \
+    io.javaoperatorsdk/primary-name=connectivity-proxy \
+    io.javaoperatorsdk/primary-namespace=kyma-system \
+
+done
+
 
 
 exit 0
