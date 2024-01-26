7ea08e98b563ff09b24e97f6dd6299daf1d8b13a
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-01-26 15:34:20 +0100

    adding ignore not found mode

--- images/cleaner/cleaner.sh
+++ images/cleaner/cleaner.sh
@@ -1,51 +1,48 @@
 #!/bin/sh
 
+set -e
+
 echo "Running Connectivity Proxy Cleanup script"
-
-echo "Waiting 10 seconds"
-
-sleep 10
-
 echo "Removing all Connectivity Proxy workloads"
 
-kubectl delete statefulset -n kyma-system connectivity-proxy
-kubectl delete deployment -n kyma-system connectivity-proxy-restart-watcher
-kubectl delete deployment -n kyma-system connectivity-proxy-sm-operator
+kubectl delete statefulset -n kyma-system connectivity-proxy --ignore-not-found
+kubectl delete deployment -n kyma-system connectivity-proxy-restart-watcher --ignore-not-found
+kubectl delete deployment -n kyma-system connectivity-proxy-sm-operator --ignore-not-found
 
 echo "Removing all Connectivity Proxy services"
 
-kubectl delete service -n kyma-system connectivity-proxy
-kubectl delete service -n kyma-system connectivity-proxy-smv
-kubectl delete service -n kyma-system connectivity-proxy-tunnel
-kubectl delete service -n kyma-system connectivity-proxy-tunnel-0
-kubectl delete service -n kyma-system connectivity-proxy-tunnel-healthcheck
+kubectl delete service -n kyma-system connectivity-proxy --ignore-not-found
+kubectl delete service -n kyma-system connectivity-proxy-smv --ignore-not-found
+kubectl delete service -n kyma-system connectivity-proxy-tunnel --ignore-not-found
+kubectl delete service -n kyma-system connectivity-proxy-tunnel-0 --ignore-not-found
+kubectl delete service -n kyma-system connectivity-proxy-tunnel-healthcheck --ignore-not-found
 
 echo "Removing all Connectivity Proxy RBAC resources"
 
-kubectl delete serviceaccount -n kyma-system connectivity-proxy-restart-watcher
-kubectl delete clusterrole -n kyma-system connectivity-proxy-restart-watcher
-kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-restart-watcher
+kubectl delete serviceaccount -n kyma-system connectivity-proxy-restart-watcher --ignore-not-found
+kubectl delete clusterrole -n kyma-system connectivity-proxy-restart-watcher --ignore-not-found
+kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-restart-watcher --ignore-not-found
 
-kubectl delete serviceaccount -n kyma-system connectivity-proxy-sm-operator
-kubectl delete clusterrole -n kyma-system connectivity-proxy-service-mappings
-kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-service-mappings
-kubectl delete mutatingwebhookconfiguration -n kyma-system connectivity-proxy-mutating-webhook-configuration
+kubectl delete serviceaccount -n kyma-system connectivity-proxy-sm-operator --ignore-not-found
+kubectl delete clusterrole -n kyma-system connectivity-proxy-service-mappings --ignore-not-found
+kubectl delete clusterrolebinding -n kyma-system connectivity-proxy-service-mappings --ignore-not-found
+kubectl delete mutatingwebhookconfiguration -n kyma-system connectivity-proxy-mutating-webhook-configuration --ignore-not-found
 
 echo "Removing all Connectivity Proxy ConfigMaps"
 
-kubectl delete configmap -n kyma-system connectivity-proxy
-kubectl delete configmap -n kyma-system connectivity-proxy-info
+kubectl delete configmap -n kyma-system connectivity-proxy --ignore-not-found
+kubectl delete configmap -n kyma-system connectivity-proxy-info --ignore-not-found
 
 echo "Removing all Connectivity Proxy Istio resources"
 
-kubectl delete envoyfilter -n istio-system connectivity-proxy-custom-protocol
-kubectl delete certificate -n istio-system cc-certs
-kubectl delete gateway -n kyma-system kyma-gateway-cc
-kubectl delete virtualservice -n kyma-system cc-proxy
-kubectl delete virtualservice -n kyma-system cc-proxy-healthcheck
-kubectl delete destinationrule -n kyma-system connectivity-proxy-tunnel-0
-kubectl delete destinationrule -n kyma-system connectivity-proxy
-kubectl delete peerauthentication -n enable-permissive-mode-for-cp
+kubectl delete envoyfilter -n istio-system connectivity-proxy-custom-protocol --ignore-not-found
+kubectl delete certificate -n istio-system cc-certs --ignore-not-found
+kubectl delete gateway -n kyma-system kyma-gateway-cc --ignore-not-found
+kubectl delete virtualservice -n kyma-system cc-proxy --ignore-not-found
+kubectl delete virtualservice -n kyma-system cc-proxy-healthcheck --ignore-not-found
+kubectl delete destinationrule -n kyma-system connectivity-proxy-tunnel-0 --ignore-not-found
+kubectl delete destinationrule -n kyma-system connectivity-proxy --ignore-not-found
+kubectl delete peerauthentication -n enable-permissive-mode-for-cp --ignore-not-found
 
 echo "Annotate all existing Connectivity Proxy Service Mappings"
 
@@ -60,6 +57,3 @@ for mapping in $mappings; do
     io.javaoperatorsdk/primary-namespace=kyma-system \
 
 done
-
-
-exit 0
