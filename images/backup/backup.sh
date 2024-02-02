e6e8081adaeb2eedc2baa3e92dcd51f0ed4d56d8
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-02-02 13:20:11 +0100

    change target namespace to connectivity-proxy-backup

--- images/backup/backup.sh
+++ images/backup/backup.sh
@@ -3,11 +3,11 @@
 #Backups connectivity-proxy, and connectivity-proxy-info config map.
 set -e
 
-BACKUP_NS=cp-backup
+BACKUP_NS=connectivity-proxy-backup
 
-echo "Running Connectivity Proxy Backup script"
+echo "Running Connectivity Proxy user configuration backup script"
 
-echo "Checking config maps for backup"
+echo "Checking if source config maps for backup exist"
 #check if connectivity-proxy and connectivity-proxy-info config maps exists in namespace kyma-system and if not, exit with 0
 if ! kubectl get cm -n kyma-system connectivity-proxy &> /dev/null; then
   echo "connectivity-proxy config map does not exist in kyma-system namespace, exiting"
@@ -19,6 +19,7 @@ if ! kubectl get cm -n kyma-system connectivity-proxy-info &> /dev/null; then
   exit 0
 fi
 
+echo "Checking if destination backup namespace for backup exist"
 # check if namespace from variable BACKUP_NS exists and if not create it in the same cluster
 if ! kubectl get namespace $BACKUP_NS &> /dev/null; then
   echo "Namespace $BACKUP_NS does not exist, creating it"
@@ -29,7 +30,7 @@ fi
 kubectl delete cm -n $BACKUP_NS connectivity-proxy --ignore-not-found
 kubectl delete cm -n $BACKUP_NS connectivity-proxy-info --ignore-not-found
 
-echo "Backing up Connectivity Proxy config maps"
+echo "Copying Connectivity Proxy config maps with user configuration to target backup namespace $BACKUP_NS"
 
 # read config maps connectivity-proxy and connectivity-proxy-info config maps from kyma-system namespace and store them in a backup namespace
 kubectl get cm -n kyma-system connectivity-proxy -o yaml | sed s/"namespace: kyma-system"/"namespace: $BACKUP_NS"/ | kubectl apply -f -
