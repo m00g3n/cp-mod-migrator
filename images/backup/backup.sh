f592f828b4c00bf4b8e5ef1727c4c099837cb67e
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-02-05 12:59:54 +0100

    fixes backup script after review

--- images/backup/backup.sh
+++ images/backup/backup.sh
@@ -26,18 +26,6 @@ if kubectl get connectivityproxies.connectivityproxy.sap.com connectivity-proxy 
 fi
 
 
-echo "Checking if source config maps for backup exist"
-#check if connectivity-proxy and connectivity-proxy-info config maps exists in namespace kyma-system and if not, exit with 0
-if ! kubectl get cm -n kyma-system connectivity-proxy &> /dev/null; then
-  echo "connectivity-proxy config map does not exist in kyma-system namespace, exiting"
-  exit 0
-fi
-
-if ! kubectl get cm -n kyma-system connectivity-proxy-info &> /dev/null; then
-  echo "connectivity-proxy-info config map does not exist in kyma-system namespace, exiting"
-  exit 0
-fi
-
 echo "Checking if destination backup namespace for backup exist"
 # check if namespace from variable BACKUP_NS exists and if not create it in the same cluster
 if ! kubectl get namespace $BACKUP_NS &> /dev/null; then
@@ -52,8 +40,20 @@ kubectl delete cm -n $BACKUP_NS connectivity-proxy-info --ignore-not-found
 echo "Copying Connectivity Proxy config maps with user configuration to target backup namespace $BACKUP_NS"
 
 # read config maps connectivity-proxy and connectivity-proxy-info config maps from kyma-system namespace and store them in a backup namespace
-kubectl get cm -n kyma-system connectivity-proxy -o yaml | sed s/"namespace: kyma-system"/"namespace: $BACKUP_NS"/ | kubectl apply -f -
-kubectl get cm -n kyma-system connectivity-proxy-info -o yaml | sed s/"namespace: kyma-system"/"namespace: $BACKUP_NS"/ | kubectl apply -f -
+
+if kubectl get cm -n kyma-system connectivity-proxy &> /dev/null; then
+  kubectl get cm -n kyma-system connectivity-proxy -o yaml | sed s/"namespace: kyma-system"/"namespace: $BACKUP_NS"/ | kubectl apply -f -
+  echo "connectivity-proxy config map backed up successfully"
+else
+  echo "Warning! connectivity-proxy config map does not exist in kyma-system namespace, skipping backup"
+fi
+
+if kubectl get cm -n kyma-system connectivity-proxy-info &> /dev/null; then
+  kubectl get cm -n kyma-system connectivity-proxy-info -o yaml | sed s/"namespace: kyma-system"/"namespace: $BACKUP_NS"/ | kubectl apply -f -
+  echo "connectivity-proxy-info config map backed up successfully"
+else
+  echo "Warning! connectivity-proxy-info config map does not exist in kyma-system namespace, skipping backup"
+fi
 
 echo "Annotate CR that backup is completed after migration"
 kubectl annotate connectivityproxies.connectivityproxy.sap.com connectivity-proxy -n kyma-system connectivityproxy\.sap\.com/backed-up="true"
