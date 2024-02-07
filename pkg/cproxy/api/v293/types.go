37e37426b23632c74bf4df49e0cb3fb0e10652a4
m00g3n <marcin.witalis@gmail.com>
2024-02-07 08:28:48 +0100

    Fix connectivity-service secret name

--- pkg/cproxy/api/v293/types.go
+++ pkg/cproxy/api/v293/types.go
@@ -1,16 +1,16 @@
 package v293
 
 const (
-	CProxyCMName                             = "connectivity-proxy"
-	CProxyCMNamespace                        = "kyma-system"
-	CProxyConfigFilename                     = "connectivity-proxy-config.yml"
-	CProxyCMInfoName                         = "connectivity-proxy-info"
-	CProxyCMInfoNamespace                    = CProxyCMNamespace
-	CProxyOnpremiseProxyHost                 = "onpremise_proxy_host"
-	CProxyOnpremiseProxyHttpPort             = "onpremise_proxy_http_port"
-	CProxyOnpremiseProxyLdapPort             = "onpremise_proxy_ldap_port"
-	CProxyOnpremiseProxyPort                 = "onpremise_proxy_port"
-	CProxyOnpremiseProxyRfcPort              = "onpremise_proxy_rfc_port"
-	CProxyOnpremiseSocks5ProxyPort           = "onpremise_socks5_proxy_port"
-	CProxyConnectivityServiceSecretName = "connectivity-service-secret"
+	CProxyCMName                        = "connectivity-proxy"
+	CProxyCMNamespace                   = "kyma-system"
+	CProxyConfigFilename                = "connectivity-proxy-config.yml"
+	CProxyCMInfoName                    = "connectivity-proxy-info"
+	CProxyCMInfoNamespace               = CProxyCMNamespace
+	CProxyOnpremiseProxyHost            = "onpremise_proxy_host"
+	CProxyOnpremiseProxyHttpPort        = "onpremise_proxy_http_port"
+	CProxyOnpremiseProxyLdapPort        = "onpremise_proxy_ldap_port"
+	CProxyOnpremiseProxyPort            = "onpremise_proxy_port"
+	CProxyOnpremiseProxyRfcPort         = "onpremise_proxy_rfc_port"
+	CProxyOnpremiseSocks5ProxyPort      = "onpremise_socks5_proxy_port"
+	CProxyConnectivityServiceSecretName = "connectivity-proxy-service-key"
 )
